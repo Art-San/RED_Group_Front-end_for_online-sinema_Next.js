58e29d1bb0102864befaff7b3506e81aeaabfe7b
Aleksander <asava@sibmail.com>
2023-10-02 14:09:23 +0700

    ok

--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	poweredByHeader: false,
+	optimizeFonts: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+		// APP-SERVER_URL: process.env.REACT_APP-SERVER_URL,
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `http://localhost:3001/api/:path*`,
+			},
+			{
+				source: '/uploads/:path*',
+				destination: `http://localhost:3001/uploads/:path*`,
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
