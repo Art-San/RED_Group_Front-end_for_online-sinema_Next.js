da60d1c8f03463bc8b7990e614bbbe872e3ed006
Aleksander <asava@sibmail.com>
2023-10-03 21:17:25 +0700

    Axios и React Query

--- front/app/components/layout/Navigation/MenuContainer/genres/usePopularGenres.ts
+++ front/app/components/layout/Navigation/MenuContainer/genres/usePopularGenres.ts
@@ -1,1 +1,12 @@
-export const usePopularGenres = () => null
+import { useQuery } from 'react-query'
+
+import { GenreService } from '@/services/genre/genre.service'
+
+export const usePopularGenres = () => {
+	const queryData = useQuery(
+		'popular genre menu',
+		() => GenreService.getPopularGenres
+	)
+
+	return queryData
+}
---
+++ front/app/services/genre/genre.service.ts
@@ -0,0 +1,16 @@
+//  в сервисах хранятся наши запросы
+import { axiosClassic } from 'api/interceptors'
+
+import { IGenre } from '@/shared/types/movie.types'
+
+import { getGenresUrl } from '@/configs/api.config'
+
+export const GenreService = {
+	async getPopularGenres(limit = 4) {
+		return axiosClassic.get<IGenre[]>(getGenresUrl('/popular'), {
+			params: {
+				limit,
+			},
+		})
+	},
+}
