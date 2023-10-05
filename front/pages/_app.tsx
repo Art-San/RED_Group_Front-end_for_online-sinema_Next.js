e38b2d23d61249482ff842a42dc505e999547ea8
Aleksander <asava@sibmail.com>
2023-10-05 23:01:54 +0700

    10. Список фильмов и 404 страница

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,12 +1,17 @@
+// import type { AppProps } from 'next/app'
+// import MainProvider from 'providers/MainProvider'
+// import '../styles/globals.scss'
+// export default function App({ Component, pageProps }: AppProps) {
+// 	return (
+// 		<MainProvider>
+// 			<Component {...pageProps} />
+// 		</MainProvider>
+// 	)
+// }
 import type { AppProps } from 'next/app'
-import MainProvider from 'providers/MainProvider'
 
 import '../styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<MainProvider>
-			<Component {...pageProps} />
-		</MainProvider>
-	)
+	return <Component {...pageProps} />
 }
