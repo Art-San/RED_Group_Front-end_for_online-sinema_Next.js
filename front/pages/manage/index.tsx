8b3131b65083af415a80f71defb7ba112f821792
Aleksander <asava@sibmail.com>
2023-10-08 12:27:07 +0700

    14. Axios interceptors

---
+++ front/pages/manage/index.tsx
@@ -0,0 +1,10 @@
+// import { NextPageAuth } from '@/shared/types/auth.types'
+import { NextPage } from 'next'
+
+const AdminPage: NextPage = () => {
+	return <div className="">Admin </div>
+}
+
+// AdminPage.isOnlyAdmin = true
+
+export default AdminPage
---
+++ front/pages/profile.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+const ProfilePage: NextPage = () => {
+	return <div className=""></div>
+}
+
+// ProfilePage.isOnlyUser = true
+
+export default ProfilePage
