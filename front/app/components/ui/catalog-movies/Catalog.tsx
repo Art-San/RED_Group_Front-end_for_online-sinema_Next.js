beccf429fcc384d40054dff54459dda738afe40e
Art San <artsan.mcc@gmail.com>
2023-11-21 23:09:21 +0700

    26. Каталоги и getStaticPaths

---
+++ front/app/components/ui/catalog-movies/Catalog.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react'
+
+import GalleryItem from '@/components/ui/gallery/GalleryItem'
+import Description from '@/components/ui/heading/Description'
+import Heading from '@/components/ui/heading/Heading'
+
+import { Meta } from '@/utils/meta/Meta'
+
+import { getMovieUrl } from '@/configs/url.config'
+
+import styles from './Catalog.module.scss'
+import { ICatalog } from './catalog.types'
+
+// 04:18
+
+const Catalog: FC<ICatalog> = ({ title, description, movies }) => {
+	return (
+		<Meta title={title} description={description}>
+			<Heading title={title} className={styles.heading} />
+			{description && (
+				<Description text={description} className={styles.description} />
+			)}
+
+			<section className={styles.movies}>
+				{movies.map((movie) => (
+					<GalleryItem
+						key={movie._id}
+						variant="horizontal"
+						item={{
+							name: movie.title,
+							posterPath: movie.bigPoster,
+							link: getMovieUrl(movie.slug),
+							content: {
+								title: movie.title,
+							},
+						}}
+					/>
+				))}
+			</section>
+
+			{/* <div className="text-center">
+				<button className={styles.button}>Load more</button>
+			</div> */}
+		</Meta>
+	)
+}
+
+export default Catalog
