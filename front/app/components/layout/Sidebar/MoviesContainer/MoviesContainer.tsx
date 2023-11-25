93dcdabc5afa40f3a8030c8ee298aab67f90eb69
Art San <artsan.mcc@gmail.com>
2023-11-25 21:59:34 +0700

    с помощью dynamic решили ошибки

--- front/app/components/layout/Sidebar/MoviesContainer/MoviesContainer.tsx
+++ front/app/components/layout/Sidebar/MoviesContainer/MoviesContainer.tsx
@@ -1,37 +1,38 @@
+// import { FC } from 'react'
+// import FavoriteMovie from './FavoriteMovie/FavoriteMovie'
+// import PopularMovie from './PopularMovie'
+// const MoviesContainer: FC = () => {
+// 	return (
+// 		<div>
+// 			<PopularMovie />
+//      <FavoriteMovie />
+// 			{/*  Error: Hydration failed because the initial UI does not match what was rendered on the server. */}
+// 			{/*Warning: Expected server HTML to contain a matching <span> in <div>.*/}
+// 			{/* See more info here: https://nextjs.org/docs/messages/react-hydration-error */}
+// 		</div>
+// 	)
+// }
+// export default MoviesContainer
+import dynamic from 'next/dynamic'
 import { FC } from 'react'
 
-import FavoriteMovie from './FavoriteMovie/FavoriteMovie'
+// import FavoriteMovie from './FavoriteMovie/FavoriteMovie'
 import PopularMovie from './PopularMovie'
+
+const DynamicFavoriteMovie = dynamic(
+	// использовали dynamic чтоб решить проблемы выше
+	() => import('./FavoriteMovie/FavoriteMovie'),
+	{
+		ssr: false,
+	}
+)
 
 const MoviesContainer: FC = () => {
 	return (
 		<div>
 			<PopularMovie />
-			<FavoriteMovie />
+			<DynamicFavoriteMovie />
 		</div>
 	)
 }
 export default MoviesContainer
-
-// import dynamic from 'next/dynamic'
-// import { FC } from 'react'
-
-// // import FavoriteMovie from './FavoriteMovie/FavoriteMovie'
-// import PopularMovie from './PopularMovie'
-
-// const DynamicFavoriteMovie = dynamic(
-// 	() => import('./FavoriteMovie/FavoriteMovie'),
-// 	{
-// 		ssr: false,
-// 	}
-// )
-
-// const MoviesContainer: FC = () => {
-// 	return (
-// 		<div>
-// 			<PopularMovie />
-// 			<DynamicFavoriteMovie />
-// 		</div>
-// 	)
-// }
-// export default MoviesContainer
