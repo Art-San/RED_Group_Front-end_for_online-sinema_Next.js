3c24686c165442bd4e5cc9e45ad05bbf4b6a6fc1
Art San <artsan.mcc@gmail.com>
2023-11-28 17:19:39 +0700

    ok

--- front/app/components/layout/Navigation/MenuContainer/MenuContainer.tsx
+++ front/app/components/layout/Navigation/MenuContainer/MenuContainer.tsx
@@ -1,8 +1,13 @@
+import dynamic from 'next/dynamic'
 import { FC } from 'react'
 
 import Menu from './Menu'
 import GenreMenu from './genres/GenreMenu'
 import { firstMenu, userMenu } from './menu.data'
+
+// const DynamicGenreMenu = dynamic(() => import('./genres/GenreMenu'), {
+// 	ssr: false,
+// })
 
 const MenuContainer: FC = () => {
 	return (
